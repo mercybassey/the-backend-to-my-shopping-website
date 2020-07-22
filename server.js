3d4a24cf769deee0b1d35a1c18686c73a3c789b1
mercybassey <udohmercy911@gmail.com>
2020-07-22 05:34:13 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import data from './data';
+
+const app = express();
+
+app.get("/api/products" , (req, res) => {
+   res.send(data.products); 
+});
+
+app.listen(5002, () => {console.log("Server started at http://localhost:5002")})
